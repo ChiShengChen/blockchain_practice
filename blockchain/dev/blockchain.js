3abee5bdf582428f764647725bbb495525e95b37
ChiShengChen <m50816m50816@gmail.com>
2018-05-31 01:33:38 +0800

    successfully built a pow blockchain with node.js

--- blockchain/dev/blockchain.js
+++ blockchain/dev/blockchain.js
@@ -1,7 +1,11 @@
+const sha256 = require('sha256'); //使用npm中sha256套件 //灌套件: npm i sha256 --save
+
 //建立blockchain物件
 function Blockchain() {
-	this.chain = [];
-	this.newTransactions = [];
+	this.chain = []; //交易區塊鍊，存取驗證成功的交易資料
+	this.pendingTransactions = []; //存取還沒驗證進練交易的資料
+
+	this.createNewBlock(100, '0', '0'); //建立Genesis Block創世區塊(nonce, previousBlockHash, hash)均可自訂
 }
 
 //建立中規中矩的bitcoin結構
@@ -9,13 +13,13 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
 	const newBlock = {
 		index: this.chain.length + 1,
 		timestamp:　Date.now(),
-		transactions: this.newTransactions,
+		transactions: this.pendingTransactions,
 		nonce: nonce,
 		hash: hash,
 		previousBlockHash: previousBlockHash
 	};
 
-	this.newTransactions = [];
+	this.pendingTransactions = [];
 	this.chain.push(newBlock);
 
 	return newBlock;
@@ -36,10 +40,39 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, recipient){
     	recipient: recipient
     };
 
-    this.newTransactions.push(newTransaction); //把東西push到第四行的newTransactions中
+    this.pendingTransactions.push(newTransaction); //把東西push到第四行的pendingTransactions中
     
-
+    return this.getLastBlock()['index'] + 1;
 }
+
+
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce){
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData); //turn all to string
+    const hash = sha256(dataAsString); //creat string
+    return hash;
+}
+
+
+//proof of work
+//這裡可以理解nonce和hash在用途上不同: 
+//BlockHash:區塊的身分編號
+//nonce:從0開始一直變，直到能在hash256中湊出0000開頭的hash值<--此即礦工的工作
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData){
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    
+    //驗證是否0000開頭
+    while (hash.substring(0, 4) !== '0000'){
+    	nonce++;
+    	hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    	console.log(hash);//把每次hash都印出來
+    }
+
+    return nonce; //nonce=電腦試過需要多少次計算才能完成一組0000開頭hash的次數
+}
+
+
+
 
 
 module.exports = Blockchain; //把整份包成一個module讓其他程式(ex: test.js)能call
--- blockchain/dev/test.js
+++ blockchain/dev/test.js
@@ -2,6 +2,69 @@ const Blockchain = require('./blockchain'); //call 同資料夾下面的blockcha
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(6666, "meow000000jkodsfljdsz", "fdskjhgdsaolt54324153");//createNewBlock = function(nonce, previousBlockHash, hash)
+/*
+
+//test1
+//交易紀錄test
+//建立沒有交易的第0區塊
+bitcoin.createNewBlock(6666, 'meow000000jk', 'fdst54324153');//createNewBlock = function(nonce, previousBlockHash, hash)  
+
+
+//第1坨交易
+bitcoin.createNewTransaction(100, 'MIKE0FD234654F4D', 'VICTOR2DF450DS2F16');
+//建立存取第1坨交易的第1區塊
+bitcoin.createNewBlock(1836, 'FF14566DSF4DSF', 'f4st5GH4A53');
+
+//第2坨交易(包含3筆)
+bitcoin.createNewTransaction(10, 'MHGJ23G654F4D', 'VICTOR2DF450DS2F16');
+bitcoin.createNewTransaction(50, 'BLIKE0FD23465496K', 'VICTOR2DF450DS2F16');
+bitcoin.createNewTransaction(200, 'AKAKE0FD234F57', 'VICTOR2DF450DS2F16');
+//建立存取第2坨交易的第2區塊
+bitcoin.createNewBlock(9865, 'JJ145R9DSF4DQ1', '095EE54FG6AM');
+
 
 console.log(bitcoin); //可以把東西在cmd視窗裡印出來，debug好用
+//console.log(bitcoin.chain[x]); //可以查看第x個區塊中transaction細節ex:一區塊中包含多筆交易
+*/
+
+
+/*
+//test2
+//test hash method
+const previousBlockHash = '0DFGAS0DSF5413DSF450DSFJ';
+const currentBlockData = [
+
+    {
+    	amount: 10,
+    	sender: 'FDG54SAD1XC0TRS',
+    	recipient: '2KJHL54E410DSF9'
+    },
+    {
+    	amount: 30,
+    	sender: '7G5546AD1XC0TRA',
+    	recipient: 'IKJHL54E41088SF9'
+    },
+    {
+    	amount: 95,
+    	sender: 'KKG54SADIN5633T',
+    	recipient: 'QWKJ8LFEL16DSF'
+    }
+
+];
+
+//const nonce = 100;
+//console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+//bitcoin.hashBlock();
+
+
+//test3
+//test proof of work
+//console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));//電腦試過需要多少次計算才能完成一組0000開頭hash的次數
+
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 18361));//把0000開頭那組印出來
+*/
+
+
+//test4
+//test genesis block
+console.log(bitcoin);
